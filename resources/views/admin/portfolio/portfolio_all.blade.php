daa3d5972e449284ac20c931ef3d556e8594e77c
Dan Kelly <dankelly2006@gmail.com>
2022-10-06 13:11:14 -0400

    Starting adding admin Portfolio

---
+++ resources/views/admin/portfolio/portfolio_all.blade.php
@@ -0,0 +1,61 @@
+@extends('admin.admin_master')
+@section('admin')
+
+    <div class="page-content">
+        <div class="container-fluid">
+
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-body">
+
+                            <h4 class="card-title">Portfolio All</h4>
+
+                            <table id="datatable" class="table table-bordered dt-responsive nowrap" style="border-collapse: collapse; border-spacing: 0; width: 100%;">
+                                <thead>
+
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Portfolio Name</th>
+                                    <th>Portfolio Title</th>
+                                    <th>Portfolio Image</th>
+                                    <th>Portfolio Description</th>
+                                    <th>Action</th>
+
+                                </tr>
+
+                                </thead>
+
+
+                                <tbody>
+                                @php($i = 1)
+                                @foreach($portfolio as $item)
+                                    <tr>
+                                        <td>{{ $i++ }}</td>
+                                        <td>{{ $item->name }}</td>
+                                        <td>{{ $item->title }}</td>
+                                        <td>{{ $item->description }}</td>
+                                        <td><img src="{{ asset($item->image) }}" style="width:60px;height:50px;"></td>
+                                        <td>
+                                            <a href="{{ route('edit.multi.image', $item->id) }}" class="btn btn-info sm" title="Edit Data">
+                                                <i class="fas fa-edit"></i>
+                                            </a>
+                                            <a href="{{ route('delete.multi.image', $item->id) }}" class="btn btn-danger sm" id="delete" title="Delete Data">
+                                                <i class="fas fa-trash-alt"></i>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+
+                        </div>
+                    </div>
+                </div> <!-- end col -->
+            </div> <!-- end row -->
+
+        </div>
+    </div>
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\Home\HomeSliderController;
 use App\Http\Controllers\Home\AboutController;
+use App\Http\Controllers\Home\PortfolioController;
 
 /*
 |--------------------------------------------------------------------------
@@ -51,6 +52,12 @@ Route::controller(AboutController::class)->group(function() {
     Route::get('edit/multi/image/{id}', 'EditMultiImage')->name('edit.multi.image');
     Route::post('update/multi/image', 'UpdateMultiImage')->name('update.multi.image');
     Route::get('delete/multi/image/{id}', 'DeleteMultiImage')->name('delete.multi.image');
+});
+
+// Portfolio All Routes
+Route::controller(PortfolioController::class)->group(function() {
+    Route::get('all/portfolio', 'index')->name('all.portfolio');
+    // Route::post('update/slider', 'store')->name('update.slider');
 });
 
 
